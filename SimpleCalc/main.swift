7f5e831f2e876dc68294cf2eb57d1b1204f7e07f
Rachel Kipps <rachelkipps@gmail.com>
2015-10-08 11:51:24 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Rachel Kipps on 10/8/15.
+//  Copyright © 2015 Rachel Kipps. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
